e927284428262306ccc79886acaf1235587ceac3
march-dave <ysangy@gmail.com>
2018-10-11 20:57:44 -0400

    add AdPerformance solidity

---
+++ contracts/DieselPrice.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.0;
+import "./oraclizeAPI_0.5.sol";
+
+contract DieselPrice is usingOraclize {
+    
+  event newOraclizeQuery(string description);
+  event newDieselPrice(string price);
+  function DieselPrice() {
+    update(); 
+  }
+  function __callback(bytes32 myid, string result) {
+    if (msg.sender != oraclize_cbAddress()) throw;
+    newDieselPrice(result);
+  }
+  function update() payable {        
+    newOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+        
+    oraclize_query("URL","xml(https://www.fueleconomy.gov/ws/rest/fuelprices).fuelPrices.diesel");
+  }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 const BetterMarket = artifacts.require("./BetterMarket.sol");
+const DieselPirce = artifacts.require("./DieselPirce.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(BetterMarket);
+  deployer.deploy(DieselPirce);
 };
 
