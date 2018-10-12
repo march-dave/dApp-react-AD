12a1ecdf6e2e073f49509ff5b78e7848f0fc4fa9
march-dave <ysangy@gmail.com>
2018-10-11 21:06:38 -0400

    constructor create

--- contracts/AdPerformance.sol
+++ contracts/AdPerformance.sol
@@ -1,5 +1,16 @@
 pragma solidity ^0.4.23;
 
 contract AdPerformance {
-        
+    address owner;
+    address beneficiary;
+    uint gweiToPayPerView;
+    string youtubeId;
+    bool withdrawn;
+
+    constructor(address _beneficiary, uint _gweiToPayPerView, string _youtubeId) public {
+        beneficiary = _beneficiary;
+        gweiToPayPerView = _gweiToPayPerView;
+        youtubeId = _youtubeId;
+    }
+
 }
