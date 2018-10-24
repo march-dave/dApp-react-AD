39c1fdc90fc37c74e026f7140082dc9773320eb8
march-dave <ysangy@gmail.com>
2018-10-23 20:45:12 -0400

    oraclize

--- contracts/DieselPrice.sol
+++ contracts/DieselPrice.sol
@@ -3,18 +3,18 @@ import "./oraclizeAPI_0.5.sol";
 
 contract DieselPrice is usingOraclize {
     
-  event newOraclizeQuery(string description);
-  event newDieselPrice(string price);
-  function DieselPrice() {
-    update(); 
-  }
-  function __callback(bytes32 myid, string result) {
-    if (msg.sender != oraclize_cbAddress()) throw;
-    newDieselPrice(result);
-  }
-  function update() payable {        
-    newOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+  // event newOraclizeQuery(string description);
+  // event newDieselPrice(string price);
+  // function DieselPrice() {
+  //   update(); 
+  // }
+  // function __callback(bytes32 myid, string result) {
+  //   if (msg.sender != oraclize_cbAddress()) throw;
+  //   newDieselPrice(result);
+  // }
+  // function update() payable {        
+  //   newOraclizeQuery("Oraclize query was sent, standing by for the answer..");
         
-    oraclize_query("URL","xml(https://www.fueleconomy.gov/ws/rest/fuelprices).fuelPrices.diesel");
-  }
+  //   oraclize_query("URL","xml(https://www.fueleconomy.gov/ws/rest/fuelprices).fuelPrices.diesel");
+  // }
 }
